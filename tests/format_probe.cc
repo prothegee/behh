f001363f154f000842f0a9ca1b4c786d8196ce45
prothegee <prothegee@proton.me>
2026-06-30 05:23:28 +0700

    add format probe for ci test

--- tests/format_probe.cc
+++ tests/format_probe.cc
@@ -2,10 +2,9 @@
 // this is intentionally mis-formatted so clang-format has something to fix.
 #include <iostream>
 
-int main() {
-    int x = 1;
-    if (x == 1) {
-        std::cout << "probe " << x << "\n";
-    }
-    return 0;
+int main(){
+int   x=1 ;
+        if( x==1 ){std::cout<<"probe "<<x<<"\n";}
+
+  return 0;
 }
