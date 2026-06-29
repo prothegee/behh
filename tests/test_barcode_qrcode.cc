4af913ab7862c9c7f1fe2667a3f7c72b2a69bba0
prothegee <prothegee@proton.me>
2026-06-30 02:47:17 +0700

    encode barcode and qrcode to svg and png
    edited:
    - use BEHH_TESTS_DIR for output paths
    - output names use dash, drop spaces

--- tests/test_barcode_qrcode.cc
+++ tests/test_barcode_qrcode.cc
@@ -4,54 +4,58 @@
 
 int main() {
 #if BEHH_USING_STB && BEHH_USING_NANOSVG && BEHH_USING_ZXING_CPP
-    // barcode
+    // barcode: encode & decode to both svg and png
     {
         std::string in_1 = "my barcode in 1";
-        std::string out_1 = "n/a";
-        std::string out_1_file = "../../../tests/my barcode out 1.svg";
         BarcodeFormat format = BarcodeFormat::Code128;
 
-        auto encode = behh::generate_functions::barqr::encode(
-            in_1,
-            out_1_file,
-            256,
-            256,
-            0,
-            format);
-        assert(encode == 1);
-        std::cout << "passed: encode barcode\n";
+        std::string out_1_svg_file = BEHH_TESTS_DIR "/my-barcode-out-1.svg";
+        std::string out_1_png_file = BEHH_TESTS_DIR "/my-barcode-out-1.png";
 
-        auto decode = behh::generate_functions::barqr::decode(
-            out_1_file,
-            out_1,
-            format);
-        assert(decode == 1);
-        std::cout << "passed: decode barcode\n";
+        auto encode_svg = behh::generate_functions::barqr::encode(in_1, out_1_svg_file, 256, 256, 0, format);
+        assert(encode_svg == 1);
+        std::cout << "passed: encode barcode svg\n";
+
+        auto encode_png = behh::generate_functions::barqr::encode(in_1, out_1_png_file, 256, 256, 0, format);
+        assert(encode_png == 1);
+        std::cout << "passed: encode barcode png\n";
+
+        std::string out_1_svg = "n/a";
+        auto decode_svg = behh::generate_functions::barqr::decode(out_1_svg_file, out_1_svg, format);
+        assert(decode_svg == 1);
+        std::cout << "passed: decode barcode svg\n";
+
+        std::string out_1_png = "n/a";
+        auto decode_png = behh::generate_functions::barqr::decode(out_1_png_file, out_1_png, format);
+        assert(decode_png == 1);
+        std::cout << "passed: decode barcode png\n";
     }
 
-    // qrcode
+    // qrcode: encode & decode to both svg and png
     {
         std::string in_1 = "my qrcode in 1";
-        std::string out_1 = "n/a";
-        std::string out_1_file = "../../../tests/my qrcode out 1.png";
         BarcodeFormat format = BarcodeFormat::QRCode;
 
-        auto encode = behh::generate_functions::barqr::encode(
-            in_1,
-            out_1_file,
-            256,
-            256,
-            0,
-            format);
-        assert(encode == 1);
-        std::cout << "passed: encode qrcode\n";
+        std::string out_1_svg_file = BEHH_TESTS_DIR "/my-qrcode-out-1.svg";
+        std::string out_1_png_file = BEHH_TESTS_DIR "/my-qrcode-out-1.png";
 
-        auto decode = behh::generate_functions::barqr::decode(
-            out_1_file,
-            out_1,
-            format);
-        assert(decode == 1);
-        std::cout << "passed: decode qrcode\n";
+        auto encode_svg = behh::generate_functions::barqr::encode(in_1, out_1_svg_file, 256, 256, 0, format);
+        assert(encode_svg == 1);
+        std::cout << "passed: encode qrcode svg\n";
+
+        auto encode_png = behh::generate_functions::barqr::encode(in_1, out_1_png_file, 256, 256, 0, format);
+        assert(encode_png == 1);
+        std::cout << "passed: encode qrcode png\n";
+
+        std::string out_1_svg = "n/a";
+        auto decode_svg = behh::generate_functions::barqr::decode(out_1_svg_file, out_1_svg, format);
+        assert(decode_svg == 1);
+        std::cout << "passed: decode qrcode svg\n";
+
+        std::string out_1_png = "n/a";
+        auto decode_png = behh::generate_functions::barqr::decode(out_1_png_file, out_1_png, format);
+        assert(decode_png == 1);
+        std::cout << "passed: decode qrcode png\n";
     }
 
 // encode decode batch?
